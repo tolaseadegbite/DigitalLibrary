a0c1812308fb94f065678e07c8a2a8c0bcb7ea5c
tolaseadegbite <tolasekelvin@gmail.com>
2023-11-11 13:44:50 +0100

    Advanced Search

--- app/models/search.rb
+++ app/models/search.rb
@@ -17,12 +17,12 @@ class Search < ApplicationRecord
     def search_resources
         resources = Resource.all
 
-        resources = resources.where(["name LIKE ?", "%#{keyword}%"]) if keyword.present?
-        resources = resources.where(["#{category_id} LIKE ?", category_id]) if category_id.present?
-        resources = resources.where(["author_id LIKE ?", author_id]) if author_id.present?
-        resources = resources.where(["publisher_id LIKE ?", publisher_id]) if publisher_id.present?
-        resources = resources.where(["resource_language_id LIKE ?", resource_language_id]) if resource_language_id.present?
-        resources = resources.where(["resource_type_id LIKE ?", resource_type_id]) if resource_type_id.present?
+        resources = resources.where("name like ?", "%#{keyword}%") if keyword.present?
+        resources = resources.where(category_id: category_id) if category_id.present?
+        resources = resources.where(author_id: author_id) if author_id.present?
+        resources = resources.where(resource_language_id: resource_language_id) if resource_language_id.present?
+        resources = resources.where(resource_type_id: resource_type_id) if resource_type_id.present?
+        resources = resources.where(publisher_id: publisher_id) if publisher_id.present?
 
         return resources
     end
